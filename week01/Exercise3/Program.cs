ffa49a5dcc0b2eb2162396041cac7b050d8b4329
baimba0891 <abaimba1@byupathway.edu>
2026-07-06 15:01:01 +0000

    commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,18 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
-
-        magicNumber == 6
-        Console.Write("What is the magic number? ");
-
-        string input = Console.ReadLine();
-        int  = int.Parse(input);
-
-
-
-
-
-
-
     }
 }
