f4185860e4006d3119f3cb27e3b573a799070fe2
baimba0891 <abaimba1@byupathway.edu>
2026-07-06 10:10:40 +0000

    File naming convention corrected.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,10 +7,10 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
 
         Console.Write("What is your first name? ");
-        string firstname = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string lastname = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
 
     }
 }
