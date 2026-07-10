55edf96342aa252d541774447fb7c4a6da65a208
baimba0891 <abaimba1@byupathway.edu>
2026-07-10 12:01:10 +0000

    Completed the Week02 activity

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -2,13 +2,11 @@ using System;
 
 public class Job
 {
-    // Member variables
-    public string _company;
     public string _jobTitle;
+    public string _company;
     public int _startYear;
     public int _endYear;
 
-    // Method to display job details
     public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
