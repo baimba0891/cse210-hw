52d4b0c5cfb7c29cc593da42b67a23c0768af5d5
baimba0891 <abaimba1@byupathway.edu>
2026-07-16 12:49:08 +0000

    Finished the Scripture Program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,51 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+
+        List<Scripture> scriptures = new List<Scripture>()
+        {
+            new Scripture(
+                new Reference("2Nephi",25,26),
+                "FAnd we talk of Christ, we rejoice in Christ, we preach of Christ, we prophesy of Christ, and we write according to our prophecies, that our children may know to what source they may look for a remission of their sins."
+            ),
+
+            new Scripture(
+                new Reference("Proverbs",3,5,6),
+                "Trust in the Lord with all thine heart and lean not unto thine own understanding In all thy ways acknowledge him and he shall direct thy paths."
+            ),
+
+            new Scripture(
+                new Reference("Mosiah",2,17),
+                "When ye are in the service of your fellow beings ye are only in the service of your God."
+            )
+        };
+
+        Random random = new Random();
+        Scripture scripture = scriptures[random.Next(scriptures.Count)];
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.WriteLine();
+            Console.Write("Press Enter to continue or type 'quit': ");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                return;
+
+            scripture.HideRandomWords(3);
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine();
+        Console.WriteLine("Congratulations! You memorized the scripture.");
     }
 }
+
+
+
